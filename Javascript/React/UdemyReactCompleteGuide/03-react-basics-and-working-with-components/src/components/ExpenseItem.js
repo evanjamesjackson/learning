59d00c9cad1a0d62829131bc788fbadd1dbf0767
Evan Jackson <evan@jacksonnet.ca>
2021-09-24 15:19:48 -0300

    Extracted ExpenseDate

--- Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
+++ Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
@@ -1,11 +1,9 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 // Our first component. A component in React is really just a special Javascript function written with JSX (Javascript with XML, or HTML)
 function ExpenseItem(props) {
   // props ("properties") are how we pass data between components
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
 
   return (
     // Note that React components must only have one root element
@@ -13,11 +11,7 @@ function ExpenseItem(props) {
     // Since this isn't really HTML, some of the keywords aren't quite the same. For instance - we have to use className instead of class
     // for CSS classes, since class itself is a reserved Javascript keyword.
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
