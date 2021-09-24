f0bdd8eeae51632e792a9e95de4fe0d367bf4636
Evan Jackson <evan@jacksonnet.ca>
2021-09-24 15:15:58 -0300

    Dynamic expense data

--- Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
+++ Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
@@ -1,11 +1,11 @@
 import "./ExpenseItem.css";
 
 // Our first component. A component in React is really just a special Javascript function written with JSX (Javascript with XML, or HTML)
-function ExpenseItem() {
-  // We can refer to Javascript variables within our HTML code using the {variableName} syntax
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  // props ("properties") are how we pass data between components
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     // Note that React components must only have one root element
@@ -13,10 +13,14 @@ function ExpenseItem() {
     // Since this isn't really HTML, some of the keywords aren't quite the same. For instance - we have to use className instead of class
     // for CSS classes, since class itself is a reserved Javascript keyword.
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
