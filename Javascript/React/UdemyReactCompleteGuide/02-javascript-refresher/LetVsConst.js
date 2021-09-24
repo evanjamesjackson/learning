2a04fabe72c37b986c3e1f7b39749db7dfb47da7
Evan Jackson <evan@jacksonnet.ca>
2021-09-24 11:37:23 -0300

    Adding LetVsConst.js

---
+++ Javascript/React/UdemyReactCompleteGuide/02-javascript-refresher/LetVsConst.js
@@ -0,0 +1,11 @@
+// JavaScript refresher
+
+// let vs const - let is for variable values that can change, const is for constant values that don't change
+var myName = "Max";
+console.log(myName);
+
+myName = "Manu";
+console.log(myName);
+
+const myOtherName = "Evan";
+myOtherName = "Riley"; //throws an error
