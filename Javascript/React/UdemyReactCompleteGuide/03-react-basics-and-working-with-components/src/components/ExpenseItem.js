a8821ce352f3837dc7ae01c573317e1b23641aca
Evan Jackson <evan@jacksonnet.ca>
2021-09-24 14:41:49 -0300

    Demonstrating injecting Javascript data into HTML

--- Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
+++ Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
@@ -2,16 +2,21 @@ import "./ExpenseItem.css";
 
 // Our first component. A component in React is really just a special Javascript function written with JSX (Javascript with XML, or HTML)
 function ExpenseItem() {
+  // We can refer to Javascript variables within our HTML code using the {variableName} syntax
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     // Note that React components must only have one root element
 
     // Since this isn't really HTML, some of the keywords aren't quite the same. For instance - we have to use className instead of class
     // for CSS classes, since class itself is a reserved Javascript keyword.
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
