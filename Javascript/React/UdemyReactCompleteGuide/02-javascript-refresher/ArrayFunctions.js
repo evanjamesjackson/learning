00409b8b0f9e4a54511b0247cb829128133110a2
Evan Jackson <evan@jacksonnet.ca>
2021-09-24 13:15:06 -0300

    Adding ArrayFunctions.js

---
+++ Javascript/React/UdemyReactCompleteGuide/02-javascript-refresher/ArrayFunctions.js
@@ -0,0 +1,4 @@
+// Array functions use the arrow function syntax to perform operations on the elements of an array
+const numbers = [1, 2, 3];
+const doubleNumArray = numbers.map((x) => x * 2);
+console.log(doubleNumArray);
