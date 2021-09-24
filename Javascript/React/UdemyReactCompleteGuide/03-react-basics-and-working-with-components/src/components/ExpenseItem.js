f1f5d46a80e099a34861f505fac2773b4c1e30a4
Evan Jackson <evan@jacksonnet.ca>
2021-09-24 14:18:04 -0300

    Adding our first custom component

---
+++ Javascript/React/UdemyReactCompleteGuide/03-react-basics-and-working-with-components/src/components/ExpenseItem.js
@@ -0,0 +1,6 @@
+// Our first component. A component in React is really just a special Javascript function.
+function ExpenseItem() {
+  return <h2>Expense item</h2>;
+}
+
+export default ExpenseItem;
