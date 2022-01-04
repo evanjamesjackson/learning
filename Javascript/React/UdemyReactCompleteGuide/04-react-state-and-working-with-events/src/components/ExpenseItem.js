59833e43caab4b9b6f9d9db39039586b85c55faa
Evan Jackson <evan@jacksonnet.ca>
2022-01-04 15:01:28 -0400

    Demonstrating state management in React

--- Javascript/React/UdemyReactCompleteGuide/04-react-state-and-working-with-events/src/components/ExpenseItem.js
+++ Javascript/React/UdemyReactCompleteGuide/04-react-state-and-working-with-events/src/components/ExpenseItem.js
@@ -1,23 +1,23 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import React, { useState } from "react";
 
-// Our first component. A component in React is really just a special Javascript function written with JSX (Javascript with XML, or HTML)
 function ExpenseItem(props) {
-  // props ("properties") are how we pass data between components
+  // In order for a React component to update dynamically, it uses the concept of "state".
+  // A state change should cause the React component to be "re-evaluated" and re-drawn on the screen - it's not enough to, say, add a variable here that is updated by a function.
+  // The useState function creates a special sort of variable that, when its value is changed, causes the overall React component to be re-evaluated.
+  const [title, setTitle] = useState(props.title); //useState accepts an initial value as an argument, and returns an array containing a "managed" variable, as well as a callback function that can be used to set the variable. We can then use array destructuring to assign the values to our own variables.
 
   const clickHandler = () => {
     console.log("Clicked");
+    setTitle("Updated"); // state update methods work by scheduling the update of the variable (and the re-rendering of the component)
   };
 
   return (
-    // Note that React components must only have one root element
-
-    // Since this isn't really HTML, some of the keywords aren't quite the same. For instance - we have to use className instead of class
-    // for CSS classes, since class itself is a reserved Javascript keyword.
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Change Title</button>
       </div>
