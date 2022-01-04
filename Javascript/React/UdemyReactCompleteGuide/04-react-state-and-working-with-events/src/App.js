0999c93f07d209d5ff8b2d43685862ccfd79ab75
Evan Jackson <evan@jacksonnet.ca>
2022-01-04 14:38:11 -0400

    Starting section 4

---
+++ Javascript/React/UdemyReactCompleteGuide/04-react-state-and-working-with-events/src/App.js
@@ -0,0 +1,13 @@
+import Expenses from "./components/Expenses";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      We use React components like custom HTML elements
+      <Expenses></Expenses>
+    </div>
+  );
+};
+
+export default App;
---
+++ Javascript/React/UdemyReactCompleteGuide/04-react-state-and-working-with-events/src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+// Our first component. A component in React is really just a special Javascript function written with JSX (Javascript with XML, or HTML)
+function ExpenseItem(props) {
+  // props ("properties") are how we pass data between components
+
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
+
+  return (
+    // Note that React components must only have one root element
+
+    // Since this isn't really HTML, some of the keywords aren't quite the same. For instance - we have to use className instead of class
+    // for CSS classes, since class itself is a reserved Javascript keyword.
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
